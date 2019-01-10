40fc998f29299f0a4290672a1cd8a8211eded150
xyz0z0 <xyz0z0.xyz@gmail.com>
2019-01-10 21:58:38 +0800

    Add flowable test

--- app/src/main/java/com/example/cheng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cheng/myapplication/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.cheng.myapplication;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import com.example.cheng.myapplication.room.Address;
 import com.example.cheng.myapplication.room.AddressDao;
-import com.example.cheng.myapplication.room.Connect;
 import com.example.cheng.myapplication.room.ConnectDao;
 import com.example.cheng.myapplication.room.DataBase;
-import com.example.cheng.myapplication.room.Detail;
 import com.example.cheng.myapplication.room.User;
 import com.example.cheng.myapplication.room.UserDao;
-import java.util.ArrayList;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
   ConnectDao connectDao;
   UserDao userDao;
 
-  Button btnDelete;
+  Button btnAddUser;
+  Button btnAdd;
+  Button btnAddFlow;
 
 
   @Override
@@ -31,39 +38,45 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    btnDelete = findViewById(R.id.btn_delete);
+    btnAddUser = findViewById(R.id.btn_add_user);
     dataBase = DataBase.getInstance(this);
     addressDao = dataBase.getAddressDao();
     connectDao = dataBase.getConnectDao();
     userDao = dataBase.getUserDao();
 
-    btnDelete.setOnClickListener(new View.OnClickListener() {
+    btnAddUser.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         new Thread(new Runnable() {
           @Override public void run() {
-            new Thread(new Runnable() {
-              @Override public void run() {
 
-                List<Address> addresses = connectDao.getAddressForUsers(1);
-                if (addresses.size() > 0) {
-                  for (Address address : addresses) {
-                    Log.d("cxg", "addresses " + address.getStreet());
-                  }
-                } else {
-                  Log.d("cxg", "addresses 为空");
-                }
-
-                List<User> users = connectDao.getUsersForAddress(1);
-                if (users.size() > 0) {
-                  for (User user : users) {
-                    Log.d("cxg", "users " + user.getFirstName());
-                  }
-                } else {
-                  Log.d("cxg", "users 为空");
-                }
-
-              }
-            }).start();
+            // List<Address> addresses = connectDao.getAddressForUsers(1);
+            // if (addresses.size() > 0) {
+            //   for (Address address : addresses) {
+            //     Log.d("cxg", "addresses " + address.getStreet());
+            //   }
+            // } else {
+            //   Log.d("cxg", "addresses 为空");
+            // }
+            //
+            // List<User> users = connectDao.getUsersForAddress(1);
+            // if (users.size() > 0) {
+            //   for (User user : users) {
+            //     Log.d("cxg", "users " + user.getFirstName());
+            //   }
+            // } else {
+            //   Log.d("cxg", "users 为空");
+            // }
+            for (int i = 0; i < 30; i++) {
+              Random r = new Random();
+              int i1 = r.nextInt(20);
+              Log.d("cxg-r", "i1 " + i1);
+            }
+            Random r = new Random();
+            int i1 = r.nextInt(20);
+            User user = new User("姓名 " + i1);
+            long[] l = userDao.insert(user);
+            Log.d("cxg", "long " + l.length);
+            Log.d("cxg", "long " + l[0]);
 
           }
         }).start();
@@ -73,20 +86,53 @@ public class MainActivity extends AppCompatActivity {
 
     new Thread(new Runnable() {
       @Override public void run() {
-        Address address = new Address("合肥1");
-        List<String> details = new ArrayList<>();
-        details.add("test1-1");
-        details.add("test1-2");
-        Detail detail = new Detail("测试1", "测试2", "测试3", details);
-        address.setDetail(detail);
-        addressDao.insert(address);
-        User user = new User("合肥1人");
-        userDao.insert(user);
-        Connect connect = new Connect(1, 2);
-        connectDao.insert(connect);
-
+        List<User> users = userDao.queryAllUser();
+        Log.d("cxg", "queryAllUser " + users.size());
       }
     }).start();
+
+    Disposable d1 =  Observable.just(1)
+        .map(new Function<Integer, Object>() {
+          @Override public Object apply(Integer integer) throws Exception {
+            List<User> list = userDao.queryAllUser();
+            Log.d("cxg","list "+list.size());
+            return 1;
+          }
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Consumer<Object>() {
+          @Override public void accept(Object o) throws Exception {
+
+          }
+        });
+
+    Disposable d = userDao.queryAllUserFlowable()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Consumer<List<User>>() {
+          @Override public void accept(List<User> users) throws Exception {
+            Log.d("cxg", "queryAllUserFlowable " + users.size());
+          }
+        });
+    // d.dispose();
+
+    // new Thread(new Runnable() {
+    //   @Override public void run() {
+    //     Address address = new Address("合肥1");
+    //     List<String> details = new ArrayList<>();
+    //     details.add("test1-1");
+    //     details.add("test1-2");
+    //     Detail detail = new Detail("测试1", "测试2", "测试3", details);
+    //     address.setDetail(detail);
+    //     addressDao.insert(address);
+    //     User user = new User("合肥1人");
+    //     userDao.insert(user);
+    //     Connect connect = new Connect(1, 2);
+    //     connectDao.insert(connect);
+    //
+    //   }
+    // }).start();
   }
 
 }
--- app/src/main/java/com/example/cheng/myapplication/room/UserDao.java
+++ app/src/main/java/com/example/cheng/myapplication/room/UserDao.java
@@ -3,6 +3,9 @@ package com.example.cheng.myapplication.room;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import io.reactivex.Flowable;
+import java.util.List;
 
 /**
  * Created by chengxg on 2019/1/10
@@ -12,5 +15,12 @@ public interface UserDao {
 
   @Insert(onConflict = OnConflictStrategy.FAIL)
   long[] insert(User... users);
+
+  // 只有返回了 Flowable 才会注册观察者，并在后续的更新中自动调用
+  @Query("SELECT * FROM user")
+  Flowable<List<User>> queryAllUserFlowable();
+
+  @Query("SELECT * FROM user")
+  List<User> queryAllUser();
 
 }
